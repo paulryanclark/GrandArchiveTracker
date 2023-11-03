8a1ead3e18e0b514c795210b21ba041f5db2cdc7
Paul Clark <paulryanclark@gmail.com>
2023-11-03 12:40:24 -0700

    Progress

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+Array.prototype.asSelector = function() {
+    return this.join('_')
+};
+
 function Player(number, damage, life, special) {
     this.number = number;
     this.damage = damage;
@@ -5,47 +9,148 @@ function Player(number, damage, life, special) {
     this.special = special;
 };
 
-Player.prototype.incrementDamange = function () {
-    this.damage += 1;
+Player.prototype.lifeLeftIdentifier = "LifeLeft";
+Player.prototype.lifeIdentifier = "Life";
+Player.prototype.specialIdentifier = "Special";
+Player.prototype.incrementIdentifier = "Increment";
+Player.prototype.decrementIdentifier = "Decrement";
+
+Player.prototype.lifeLeft = function() {
+    return this.life - this.damage;
 };
 
-Player.prototype.applyPlayerModel = function() {
-    const playerSelector = "#Player" + this.number;
-    const damageElement = document.querySelector(playerSelector + "_Damage");
-    damageElement.textContent = this.damage.toString();
-    const lifeElement = document.querySelector(playerSelector + "_Life");
+Player.prototype.attachToDocument = function() {
+    this.applyToDocument();
+    this.createEventListeners();
+};
+
+Player.prototype.applyToDocument = function() {
+    const lifeLeftElement = this.lifeLeftElement();
+    lifeLeftElement.textContent = this.lifeLeft().toString();
+    const lifeElement = this.lifeElement();
     lifeElement.textContent = this.life.toString();
+    const specialElement = this.specialElement();
+    specialElement.textContent = this.special.toString();
+};
+
+Player.prototype.createEventListeners = function() {
+    var self = this;
+    this.lifeLeftIncrementElement().addEventListener('click', function() {
+        self.onLifeLeftIncrement();
+    });
+    this.lifeLeftDecrementElement().addEventListener('click', function() {
+        self.onLifeLeftDecrement();
+    });
+    this.lifeDecrementElement().addEventListener('click', function() {
+        self.onLifeDecrement();
+    });
+    this.lifeIncrementElement().addEventListener('click', function() {
+        self.onLifeIncrement();
+    });
+    this.specialDecrementElement().addEventListener('click', function() { 
+        self.onSpecialDecrement();
+    });
+    this.specialIncrementElement().addEventListener('click', function() {
+        self.onSpecialIncrement();
+    });
+};
+
+Player.prototype.onLifeLeftIncrement = function() {
+    if(this.damage > 0) {
+        this.damage -= 1;
+        this.applyToDocument();
+    }
+};
+
+Player.prototype.onLifeLeftDecrement = function() {
+    this.damage += 1;
+    this.applyToDocument();
+};
+
+Player.prototype.onLifeIncrement = function() {
+    this.life += 1;
+    this.applyToDocument();
+};
+
+Player.prototype.onLifeDecrement = function() {
+    if(this.life > 0) {
+        this.life -= 1;
+        this.applyToDocument();
+    }
+};
+
+Player.prototype.onSpecialIncrement = function() {
+    this.special += 1;
+    this.applyToDocument();
+};
+
+Player.prototype.onSpecialDecrement = function() {
+    if(this.special > 0) {
+        this.special -= 1;
+        this.applyToDocument();
+    }
+};
+
+Player.prototype.playerSelector = function() {
+    return "#Player" + this.number;
+};
+
+Player.prototype.lifeLeftElement = function() {
+    const selector = [this.playerSelector(), this.lifeLeftIdentifier].asSelector();
+    console.log(selector)
+    return document.querySelector(selector);
+};
+
+Player.prototype.lifeLeftIncrementElement = function() {
+    const selector = [this.playerSelector(), this.lifeLeftIdentifier, this.incrementIdentifier].asSelector();
+    return document.querySelector(selector);
+};
+
+Player.prototype.lifeLeftDecrementElement = function() {
+    const selector = [this.playerSelector(), this.lifeLeftIdentifier, this.decrementIdentifier].asSelector();
+    return document.querySelector(selector);
+};
+
+Player.prototype.lifeElement = function() {
+    const selector = [this.playerSelector(), this.lifeIdentifier].asSelector();
+    return document.querySelector(selector);
+};
+
+Player.prototype.lifeIncrementElement = function() {
+    const selector = [this.playerSelector(), this.lifeIdentifier, this.incrementIdentifier].asSelector();
+    return document.querySelector(selector);
+};
+
+Player.prototype.lifeDecrementElement = function() {
+    const selector = [this.playerSelector(), this.lifeIdentifier, this.decrementIdentifier].asSelector();
+    return document.querySelector(selector);
+};
+
+Player.prototype.specialElement = function() {
+    const selector = [this.playerSelector(), this.specialIdentifier].asSelector();
+    return document.querySelector(selector);
+};
+
+Player.prototype.specialIncrementElement = function() {
+    const selector = [this.playerSelector(), this.specialIdentifier, this.incrementIdentifier].asSelector();
+    return document.querySelector(selector);
+};
+
+Player.prototype.specialDecrementElement = function() {
+    const selector = [this.playerSelector(), this.specialIdentifier, this.decrementIdentifier].asSelector();
+    return document.querySelector(selector);
 };
 
 const player1 = new Player(1, 0, 15, 0);
 const player2 = new Player(2, 0, 15, 0);
 
-function applyDataModel() {
-    player1.applyPlayerModel()
-    player2.applyPlayerModel()
-};
-
-function applyPlayerModel(playerNumber) {
-    const player = dataModel.player[playerNumber];
-    const playerSelector = "#Player" + playerNumber;
-
-    const damageElement = document.querySelector(playerSelector + "_Damage");
-    damageElement.textContent = player.damage.toString();
-    const lifeElement = document.querySelector(playerSelector + "_Life");
-    lifeElement.textContent = player.life.toString();
-};
-
-function createEventListeners() {
-    self.createPlayerEventListener(1);
-    self.createPlayerEventListener(2);
-};
-
-function createPlayerEventListener(playerNumber) {
-
+function onLoad() {
+    player1.attachToDocument()
+    player2.attachToDocument()
 };
 
 window.onload = function () {
-    applyDataModel();
+    onLoad();
 };
 
 
